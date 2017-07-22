051698f290e09d356443a97cfc4f1aab4d527ba0
Jaymin Panchal <jayminpanchal10@gmail.com>
2017-07-22 09:53:59 +0530

    Receiver project added with setup

---
+++ MapReceiverProject/index.android.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class MapReceiverProject extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('MapReceiverProject', () => MapReceiverProject);
