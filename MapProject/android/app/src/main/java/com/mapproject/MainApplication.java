8e036f07df25d46a52974a27d7b577e20adf5c1e
Jaymin Panchal <jayminpanchal10@gmail.com>
2017-07-24 12:10:51 +0530

    Added react navigation library

--- MapProject/android/app/src/main/java/com/mapproject/MainApplication.java
+++ MapProject/android/app/src/main/java/com/mapproject/MainApplication.java
@@ -2,7 +2,10 @@ package com.mapproject;
 
 import android.app.Application;
 
+import android.support.annotation.Nullable;
+
 import com.facebook.react.ReactApplication;
+import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -11,30 +14,18 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends NavigationApplication{
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+      @Override
+      public boolean isDebug() {
+          return BuildConfig.DEBUG;
+      }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
-  };
+      @Nullable
+      @Override
+  public List<ReactPackage> createAdditionalReactPackages()
+  {
+          return null;
+      }
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
