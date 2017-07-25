84d84add0123856507d3b2291b1b1d3c1c3a2adc
Jaymin Panchal <jayminpanchal10@gmail.com>
2017-07-25 13:53:57 +0530

    Changed real time location with map center and circle

--- MapProject/src/screens/Home.js
+++ MapProject/src/screens/Home.js
@@ -42,13 +42,19 @@ export default class Home extends React.Component {
     componentDidMount() {
         getUser().then(data => {
             this.userId = JSON.parse(data).id;
+            this.token = JSON.parse(data).token;
             this.startConnection();
         });
     }
 
     startConnection() {
-        console.log("Home user Id:" + this.userId);
-        this.ds = deepstream(WS_URL);
+        console.log("Home user Id:" + this.userId + " " + this.token);
+        this.ds = deepstream(WS_URL).login({
+            "token": this.token
+        }, (success, data) => {
+            console.log(success);
+            console.log(data);
+        });
         this.ds.on('error', (err, event, topic) => {
             console.log("Error ws");
             console.log(err);
@@ -72,9 +78,19 @@ export default class Home extends React.Component {
         console.log("record check completed");
         // the set() method allows us to now set data.
         this.record.set('username', this.userId);
-        this.callback(true);
-        alert("on record check");
-        navigator.geolocation.watchPosition(this.onPositionUpdate.bind(this));
+        //this.callback(true);
+        //alert("on record check");
+        //navigator.geolocation.watchPosition(this.onPositionUpdate.bind(this));
+
+        this.watchId = navigator.geolocation.watchPosition(
+            this.onPositionUpdate.bind(this),
+            (error) => console.log(JSON.stringify(error)),
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10}
+        );
+    }
+
+    componentWillUnmount() {
+        navigator.geolocation.clearWatch(this.watchId);
     }
 
     onPositionUpdate(position) {
@@ -85,27 +101,47 @@ export default class Home extends React.Component {
         };
         this.record.set('position', this.pos);
         //creates the list that contains our latitude and longitude
-        this.list = ds.record.getList('users_within_radius/' + this.pos.lat + '/' + this.pos.lng + '/' + 1 + '/' + this.username)
+        this.list = this.ds.record.getList('users_within_radius/' + this.pos.lat + '/' + this.pos.lng + '/' + 1 + '/' + this.userId);
         this.list.subscribe(this._onGetEntries.bind(this));
         //setCenter is a method called to find the center for the map,
         //that was created with google maps api
 
-        let region = this.state.region;
-        region.latitude = position.coords.latitude;
-        region.longitude = position.coords.longitude;
-        region.latitudeDelta = 0.0922;
-        region.longitudeDelta = 0.0421;
+        let newRegion = {...this.state.region};
+        newRegion.latitude = this.pos.lat;
+        newRegion.longitude = this.pos.lng;
+        newRegion.latitudeDelta = 0.0922;
+        newRegion.longitudeDelta = 0.0421;
 
-        let circleCenter = this.state.circleCenter;
-        circleCenter.latitude = position.coords.latitude;
-        circleCenter.longitude = position.coords.longitude;
-
-        this.setState({region: region});
-        this.setState({circleCenter: circleCenter});
-
-        console.log(JSON.stringify(this.state.region));
-
+        let newCircleCenter = {...this.state.circleCenter};
+        newCircleCenter.latitude = this.pos.lat;
+        newCircleCenter.longitude = this.pos.lng;
+        this.setState({region: newRegion});
+        this.setState({circleCenter: newCircleCenter});
     }
+
+    _onGetEntries(users) {
+        let recordNames = this.list.getEntries();
+        this._updateMarkers(recordNames);
+    }
+
+    _updateMarkers(userRecordNames) {
+        console.log(JSON.stringify(userRecordNames));
+        /*for (let i = 0; i < userRecordNames.length; i++) {
+            if (!this.markers[userRecordNames[i]]) {
+                //for each list entry, we create a new marker instance,
+                //where we can subscribe to the record corresponding with the list entry
+                this.markers[userRecordNames[i]] = new Marker(userRecordNames[i], this.map, this.username);
+            }
+        }
+
+        for (let userRecordName in this.markers) {
+            if (userRecordNames.indexOf(userRecordName) === -1) {
+                this.markers[userRecordName].destroy();
+                delete this.markers[userRecordName];
+            }
+        }*/
+    }
+
 }
 const styles = StyleSheet.create({
     container: {
